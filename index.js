2d29bc564549e930fec34858598bd016f546cdc7
xionter <andrey0502255@gmail.com>
2026-03-10 17:36:32 +0500

    animaster

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 5000);
+        });
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
         });
 }
 
@@ -54,6 +59,12 @@ function animaster() {
         element.classList.add('show');
     }
 
+    function fadeOut(element, duration) {
+        element.style.transitionDuration =  `${duration}ms`;
+        element.classList.remove('show');
+        element.classList.add('hide');
+    }
+
     /**
      * Функция, увеличивающая/уменьшающая элемент
      * @param element — HTMLElement, который надо анимировать
@@ -63,5 +74,17 @@ function animaster() {
     function scale(element, duration, ratio) {
         element.style.transitionDuration =  `${duration}ms`;
         element.style.transform = getTransform(null, ratio);
+    }
+
+    function resetFadeIn(element) {
+        element.style.transitionDuration = null;
+        element.style.show = null;
+        element.classList.remove('hide');
+        element.classList.add('show');
+
+    }
+    function resetFadeOut(element) {
+    }
+    function resetMoveAndScale(element) {
     }
 }
